79bd6b531600d718efaaed4e04faba73c2a9e792
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-16 23:04:37 +0530

    changes

--- src/utils/useaxios.js
+++ src/utils/useaxios.js
@@ -7,5 +7,26 @@ import { AuthContext } from "../context/AuthContext";
 const baseURL = "http://127.0.0.1:8000/api";
 
 const useAxios = () => {
-    const {authTokens , setUser , setAuthTokens}= useContext(AuthContext);
-}
+  const { authTokens, setUser, setAuthTokens } = useContext(AuthContext);
+
+  const axiosInstance = axios.create({
+    baseURL,
+    headers: {
+      Authorization: `Bearer ${authTokens?.access}`,
+    },
+  }).access;
+
+  axiosInstance.interceptors.request.use(async (req) => {
+    const user = jwt_decode(authTokens);
+
+    const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
+    if (isExpired) return req;
+
+    const response = await axios.post(`${baseURL}/token/refresh/`, {
+      refresh: authTokens.refresh,
+    });
+
+    localStorage.setItem('authToken' , JSON.stringify(response.data))
+  });
+};
